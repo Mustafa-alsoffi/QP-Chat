a54e3402ff80bba362d2c1d4d34698ec4394cbd6
Mustafa Alsoffi <alsoffimustafa@gmail.com>
2020-07-31 06:49:40 +0800

    Add text and style to index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,8 +4,11 @@ import styles from "../styles/Home.module.css";
 export default function Home() {
   return (
     <div className="container-fluid d-flex justify-content-center" style={{minHeight: '100vh'}}>
-      <div className="card my-auto w-50 p-3">
-        
+      <div className="card my-auto p-3">
+      <div className="card-body">
+    <h5 className="card-title text-center">Welcome to QP Chat!</h5>
+    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
           <button type="button" className="btn btn-primary m-1" style={{minHeight: '50px'}}>
             Login
           </button>

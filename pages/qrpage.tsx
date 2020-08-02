31c38f41c669a60f42c8a4cbb83ffcddd24a8279
Mustafa Alsoffi <alsoffimustafa@gmail.com>
2020-08-02 22:11:08 +0800

    Add QR canvas to QR page

--- pages/qrpage.tsx
+++ pages/qrpage.tsx
@@ -1,16 +1,30 @@
-import React from "react";
-import QRCode from 'qrcode';
+import React, { useEffect, useState, useRef } from "react";
+import QRCode from "qrcode";
+
 export default function qrpage() {
+
+
+  useEffect(() => {
+    QRCode.toCanvas(document.getElementById('canvas'),
+    'sample text', { toSJISFunc: QRCode.toSJIS }, function (error) {
+    if (error) console.error(error)
+    console.log('success!')
+  })
+  }, [])
+
+
   return (
     <div>
       <div
         className="container-fluid d-flex justify-container-center"
         style={{ minHeight: "100vh" }}
       >
-          <div>
-           <h1 className='text-right'>QR page</h1>   
-          </div>
-        
+        <div>
+          <h1>QR Image</h1>
+          <canvas
+            id='canvas'
+          ></canvas>
+        </div>
       </div>
     </div>
   );

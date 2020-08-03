fabe930cd0c97cc2d02d74af753f4ff715483f04
Mustafa Alsoffi <alsoffimustafa@gmail.com>
2020-08-04 00:59:50 +0800

    Make QR width responsive

--- pages/qrpage.tsx
+++ pages/qrpage.tsx
@@ -11,9 +11,46 @@ import {
   Tooltip,
   Overlay,
 } from "react-bootstrap";
-import { eventNames } from "process";
+import Navbarr from "../components/Navbar";
+
 export default function qrpage() {
   const [show, setShow] = useState(false);
+  if (show) {
+    setTimeout(function () {
+      setShow(false);
+    }, 1500);
+  }
+
+        const [width, setWidth] = useState(global.innerWidth);
+    const breakpoint = 620;
+    
+
+    useEffect(function mount() {
+      const handleWindowResize = () => setWidth(window.innerWidth);
+      window.addEventListener("resize", handleWindowResize);
+      console.log('here is your width ' + width);
+        
+      return function unMount() {
+        window.removeEventListener("resize", handleWindowResize);
+      };
+    });
+ 
+
+
+  useEffect(() => {
+      const setViewSize = () => width > breakpoint ? 350 : 300;
+
+    QRCode.toCanvas(
+      document.getElementById("canvas"),
+      "sample text",
+      { width: setViewSize()},
+      function (error) {
+        if (error) console.error(error);
+        console.log("success!");
+      }
+    );
+  });
+
   const target = useRef(null);
   const textAreaRef = useRef(null);
 
@@ -21,71 +58,62 @@ export default function qrpage() {
     textAreaRef.current.select();
     document.execCommand("copy");
   }
-  if (show) {
-    setTimeout(function () {
-      setShow(false);
-    }, 1500);
-  }
-  useEffect(() => {
-    QRCode.toCanvas(
-      document.getElementById("canvas"),
-      "sample text",
-      { width: "350" },
-      function (error) {
-        if (error) console.error(error);
-        console.log("success!");
-      }
-    );
-  }, []);
 
   return (
-    <div
-      className="container-fluid d-flex justify-content-center"
-      style={{ minHeight: "100vh" }}
-    >
-      <div className="card my-auto">
-        <div className="card-header text-center">Scan QR</div>
+    <>
+      <Navbarr />
+      <div
+        className="container-fluid d-flex justify-content-center"
+        style={{ minHeight: "100vh" }}
+      >
+        <div className="card my-auto">
+          <div className="card-header text-center">Scan QR</div>
 
-        <canvas id="canvas"></canvas>
-        <div className="card-body">
-          <Form>
-            <FormGroup as={Row} controlId="formPlaintext">
-              <FormLabel
-                style={{ width: 'fit-content' }}
-                column
-                xs={1}
-                className="text-center"
-              >
-                Link
-              </FormLabel>
-              <Col xs={6}>
-                <FormControl
-                  ref={textAreaRef}
-                  plaintext
-                  readOnly
-                  defaultValue=" A link here "
-                />
-              </Col>
-
-              <Col xs={1}>
-                <Button
-                  ref={target}
-                  onClick={(event) => {
-                    setShow(true);
-                    copyToClipboard(event);
-                  }}
+          <canvas id="canvas"></canvas>
+          <div className="card-body">
+            <Form>
+              <FormGroup as={Row} controlId="formPlaintext">
+                <FormLabel
+                  style={{ width: "fit-content" }}
+                  column
+                  xs={1}
+                  className="text-center"
                 >
-                  Copy
-                </Button>
-              </Col>
-            </FormGroup>
-          </Form>
-        </div>
+                  Link
+                </FormLabel>
+                <Col xs={6}>
+                  <FormControl
+                    ref={textAreaRef}
+                    plaintext
+                    readOnly
+                    defaultValue=" A link here "
+                  />
+                </Col>
 
-        <Overlay target={target.current} show={show} placement="right">
-          {(props) => <Tooltip {...props}>Copied!</Tooltip>}
-        </Overlay>
+                <Col xs={1}>
+                  <Button
+                    ref={target}
+                    onClick={(event) => {
+                      setShow(true);
+                      copyToClipboard(event);
+                    }}
+                  >
+                    Copy
+                  </Button>
+                </Col>
+              </FormGroup>
+            </Form>
+          </div>
+
+          <Overlay target={target.current} show={show} placement="right">
+            {(props) => (
+              <Tooltip id="tooltip" {...props}>
+                Copied!
+              </Tooltip>
+            )}
+          </Overlay>
+        </div>
       </div>
-    </div>
+    </>
   );
 }

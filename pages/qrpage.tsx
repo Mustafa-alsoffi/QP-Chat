f4eae0e90b14eb15504e528ce8f730920b96b7cc
Mustafa Alsoffi <alsoffimustafa@gmail.com>
2020-08-03 08:55:27 +0800

    Add QR to bootstrap card and add copy button for link

--- pages/qrpage.tsx
+++ pages/qrpage.tsx
@@ -1,30 +1,90 @@
-import React, { useEffect, useState, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import QRCode from "qrcode";
-
+import {
+  Form,
+  FormGroup,
+  FormControl,
+  Row,
+  FormLabel,
+  Col,
+  Button,
+  Tooltip,
+  Overlay,
+} from "react-bootstrap";
+import { eventNames } from "process";
 export default function qrpage() {
+  const [show, setShow] = useState(false);
+  const target = useRef(null);
+  const textAreaRef = useRef(null);
 
-
+  function copyToClipboard(e) {
+    textAreaRef.current.select();
+    document.execCommand("copy");
+  }
+  if (show) {
+    setTimeout(function () {
+      setShow(false);
+    }, 1500);
+  }
   useEffect(() => {
-    QRCode.toCanvas(document.getElementById('canvas'),
-    'sample text', { toSJISFunc: QRCode.toSJIS }, function (error) {
-    if (error) console.error(error)
-    console.log('success!')
-  })
-  }, [])
-
+    QRCode.toCanvas(
+      document.getElementById("canvas"),
+      "sample text",
+      { width: "350" },
+      function (error) {
+        if (error) console.error(error);
+        console.log("success!");
+      }
+    );
+  }, []);
 
   return (
-    <div>
-      <div
-        className="container-fluid d-flex justify-container-center"
-        style={{ minHeight: "100vh" }}
-      >
-        <div>
-          <h1>QR Image</h1>
-          <canvas
-            id='canvas'
-          ></canvas>
+    <div
+      className="container-fluid d-flex justify-content-center"
+      style={{ minHeight: "100vh" }}
+    >
+      <div className="card my-auto">
+        <div className="card-header text-center">Scan QR</div>
+
+        <canvas id="canvas"></canvas>
+        <div className="card-body">
+          <Form>
+            <FormGroup as={Row} controlId="formPlaintext">
+              <FormLabel
+                style={{ width: 'fit-content' }}
+                column
+                xs={1}
+                className="text-center"
+              >
+                Link
+              </FormLabel>
+              <Col xs={6}>
+                <FormControl
+                  ref={textAreaRef}
+                  plaintext
+                  readOnly
+                  defaultValue=" A link here "
+                />
+              </Col>
+
+              <Col xs={1}>
+                <Button
+                  ref={target}
+                  onClick={(event) => {
+                    setShow(true);
+                    copyToClipboard(event);
+                  }}
+                >
+                  Copy
+                </Button>
+              </Col>
+            </FormGroup>
+          </Form>
         </div>
+
+        <Overlay target={target.current} show={show} placement="right">
+          {(props) => <Tooltip {...props}>Copied!</Tooltip>}
+        </Overlay>
       </div>
     </div>
   );

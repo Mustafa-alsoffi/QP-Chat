64d88cfe1003d402a4210d2a61ba8b7b6378314b
Mustafa Alsoffi <alsoffimustafa@gmail.com>
2020-08-04 19:10:00 +0800

    Return null when user is not logged in

--- pages/qrpage.tsx
+++ pages/qrpage.tsx
@@ -2,19 +2,26 @@
 
 import Navbarr from "../components/Navbar";
 import QRCard from "../components/QRCard";
+import { useFetchUser } from "../utils/user";
 
 export default function qrpage() {
   
-
+  const { user, loading } = useFetchUser();
   return (
     <>
-      <Navbarr />
-      <div
-        className="container-fluid d-flex justify-content-center"
-        style={{ minHeight: "100vh" }}
-      >
-      <QRCard />
-      </div>
-    </>
+     <Navbarr />
+    {user && !loading ? 
+      (
+       
+        <div
+          className="container-fluid d-flex justify-content-center"
+          style={{ minHeight: "100vh" }}
+        >
+        <QRCard />
+        </div>
+      )
+      :
+      <h1>Error</h1>}
+      </>
   );
 }
